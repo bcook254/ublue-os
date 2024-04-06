7c8a9a4282aa9036697ace126a9e829947fccb79
Benjamin Cook <bcook254@abcook.me>
2024-04-06 17:52:06 -0600

    Add git-credential-manager package
    Downloads and installs gcm from the official GitHub release page.

--- workarounds.sh
+++ workarounds.sh
@@ -5,5 +5,7 @@ set -oeux pipefail
 # Alternatives cannot create symlinks on its own during a container build
 ln -sf /usr/bin/step-cli /etc/alternatives/step && ln -sf /etc/alternatives/step /usr/bin/step
 
+ln -sf /usr/bin/git-credential-manager /usr/lib/gcm/git-credential-manager
+
 # Step CLI RPM does not create zsh completions
 /usr/bin/step-cli completion zsh > /usr/share/zsh/site-functions/_step
