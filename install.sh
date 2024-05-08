53c1af7f8709eb9bcb12ff7dc13e6e304671dea8
Benjamin Cook <bcook254@abcook.me>
2024-05-08 16:23:03 -0600

    Cleanup curl outputs
    Hide curl console output. Make NesloLGS-NF font filenames more readable.

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -oeux pipefail
 
 # Install extras fonts
 # MesloLGS NF (used for p10k)
 mkdir -p /usr/share/fonts/meslolgs-nf
-curl --output-dir /usr/share/fonts/meslolgs-nf -LO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-curl --output-dir /usr/share/fonts/meslolgs-nf -LO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-curl --output-dir /usr/share/fonts/meslolgs-nf -LO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-curl --output-dir /usr/share/fonts/meslolgs-nf -LO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+curl --output-dir /usr/share/fonts/meslolgs-nf -sLo "MesloLGS-NF-Regular.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+curl --output-dir /usr/share/fonts/meslolgs-nf -sLo "MesloLGS-NF-Bold.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+curl --output-dir /usr/share/fonts/meslolgs-nf -sLo "MesloLGS-NF-Italic.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+curl --output-dir /usr/share/fonts/meslolgs-nf -sLo "MesloLGS-NF-Bold-Italic.ttf" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 fc-cache --system-only --really-force --verbose
 
 # Setup packages
@@ -20,14 +20,14 @@ fc-cache --system-only --really-force --verbose
 # Install rbw
 # https://github.com/doy/rbw
 mkdir /tmp/rbw
-curl -Lo /tmp/rbw/rbw_linux_amd64.tar.gz https://github.com/doy/rbw/releases/download/1.9.0/rbw_1.9.0_linux_amd64.tar.gz
+curl -sLo /tmp/rbw/rbw_linux_amd64.tar.gz https://github.com/doy/rbw/releases/download/1.9.0/rbw_1.9.0_linux_amd64.tar.gz
 tar -C /tmp/rbw -xf /tmp/rbw/rbw_linux_amd64.tar.gz rbw rbw-agent
 cp /tmp/rbw/rbw /tmp/rbw/rbw-agent /usr/bin
 
 # Install git-credential-manager
 # https://github.com/git-ecosystem/git-credential-manager
 mkdir /tmp/gcm
-curl -Lo /tmp/gcm/gcm-linux_amd64.tar.gz https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.4.1/gcm-linux_amd64.2.4.1.tar.gz
+curl -sLo /tmp/gcm/gcm-linux_amd64.tar.gz https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.4.1/gcm-linux_amd64.2.4.1.tar.gz
 tar -C /tmp/gcm -xf /tmp/gcm/gcm-linux_amd64.tar.gz
 mkdir /usr/lib/gcm
 cp /tmp/gcm/git-credential-manager /tmp/gcm/libHarfBuzzSharp.so /tmp/gcm/libSkiaSharp.so /usr/lib/gcm/
