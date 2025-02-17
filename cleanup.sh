ae40c965b5e88d1060155f8b2e4e958abb9cbdc3
Benjamin Cook <bcook254@gmail.com>
2025-02-17 10:34:05 -0700

    Remove non-empty directories

--- cleanup.sh
+++ cleanup.sh
@@ -4,5 +4,5 @@ set -eoux pipefail
 shopt -s extglob
 
 rm -rf /tmp/* || true
-find /var/lib /var/cache -maxdepth 1 -mindepth 1 ! -wholename /var/lib/alternatives ! -wholename /var/cache/rpm-ostree -type d -delete
-find /var -maxdepth 1 -mindepth 1 ! -wholename /var/lib ! -wholename /var/cache -type d -delete
+find /var/lib /var/cache -maxdepth 1 -mindepth 1 ! -wholename /var/lib/alternatives ! -wholename /var/cache/rpm-ostree -type d -exec rm -rv {} +
+find /var -maxdepth 1 -mindepth 1 ! -wholename /var/lib ! -wholename /var/cache -type d -exec rm -rv {} +
